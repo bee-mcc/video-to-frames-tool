6dc76418b82c68d1c3e813e8f93f2d68918baa25
bee-mcc <tuj03791@temple.edu>
2023-09-10 16:17:27 -0400

    MVP

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # video-to-frames-tool
+
 This is a tool to create videos by selecting and lengthening the duration of random frames from a source video
+
+## Use cases and examples
+
+This program can create some pretty trippy videos from old media. See example 1, example 2, example 3. Furthermore, this tool could be enhanced to "comic-i-fy" any type of video.
+
+## Instructions
+
+1. Install dependencies
+
+2. Configure the program to take in your source video and run main.py
+
+3. Rip audio from your source video using ffmpeg
+
+4. Collate frame output into video using ffmpeg (after getting details from source video)
+
+## Useful commands
+
+Information about file
+`ffprobe`
+
+Rip audio from file (check audio codec and associated file extension first)
+` ffmpeg -i example.mkv -vn -acodec copy output-audio.eac3`
+
+Collate frames into video (check video codec, framerate, and pixel format to use)
+`ffmpeg -framerate 30 -i 'frame%d.jpg' \ -i output-audio.eac3 -c:a copy -shortest -c:v h264 -pix_fmt yuv420p out.mp4`
+
+Can also use glob matching if output frame images in different format
+
+`ffmpeg -framerate 30 -pattern_type glob -i 'frame*.jpg' \ -i output-audio.eac3 -c:a copy -shortest -c:v h264 -pix_fmt yuv420p out.mp4`
--- main.py
+++ main.py
@@ -1,13 +1,48 @@
+
+import random
+import shutil
 import cv2
+
+def overwrite_file_content(source_file_path, destination_file_path):
+  try:
+      # Copy the contents of the source file to the destination file
+      shutil.copy2(source_file_path, destination_file_path)
+      print(f"File '{destination_file_path}' has been overwritten with contents from '{source_file_path}'.")
+  except FileNotFoundError:
+      print("One or both of the specified files does not exist.")
+  except Exception as e:
+      print(f"An error occurred: {e}")
+
 vidcap = cv2.VideoCapture('example.mkv')
 success,image = vidcap.read()
 count = 0
 while success:
   cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
-  print('Read a new frame: ', success)
   count += 1
 
-#now, take all frames and choose random one to extend. Then replace subsequent frames with chosen frame. Then choose a new one
+print(f'Files printed:{str(count)}')
+
+# now, take all frames and choose random one to extend. Then replace subsequent frames with chosen frame. Then choose a new one
+total = count
+
+count = 1
+
+while count != total:
+  numberOfFramesToExtend = random.randint(12, min(48, total - count))
+  extendedFrames = 0
+
+  while extendedFrames != numberOfFramesToExtend:
+    
+    extendedFrames += 1
+    frameToExtend = extendedFrames + count
+
+    countString = str(count)
+    frameToExtendString = str(frameToExtend)
+    
+    overwrite_file_content(f"frame{countString}.jpg", f"frame{frameToExtendString}.jpg")
+
+  count = count + numberOfFramesToExtend + 1
+
 
 #then splice all replaced frames together one after eachother and AUDIO!
